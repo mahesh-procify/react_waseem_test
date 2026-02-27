347b3baf74cbb7b736d5f1274a83632d7ae82a8d
mahesh-procify <mahesh.jain@procifynow.com>
2026-02-27 17:53:27 +0530

    changes comminted

--- util/AppLifeCycleEvent.ts
+++ util/AppLifeCycleEvent.ts
@@ -1,6 +1,6 @@
 export class AppLifeCycleEvent {
 	public onAppStart(): void {
-		alert("App started"); // Event logic here
+		alert("App started in react"); // Event logic here
 	}
 	public onBackground() {
 		alert("App in background"); // Event logic here
